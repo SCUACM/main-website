bf6617866bd64d193dcab392624cd117c2bbe141
Tim Shur <tim.shur@gmail.com>
2018-01-26 16:20:07 -0800

    Calendar styling changes

--- fullcal/init-cal.js
+++ fullcal/init-cal.js
@@ -5,8 +5,9 @@ $(document).ready(function() {
 	header: {
 	  left: 'prev,next today',
 	  center: 'title',
-	  right: 'agendaWeek,month,listYear'
+	  right: 'agendaWeek,month'
 	},
+
 	minTime: '08:00:00',
 	maxTime: '22:00:00',
 	navLinks: true,
@@ -18,10 +19,13 @@ $(document).ready(function() {
 	eventSources: [
 		{
 			googleCalendarId: 'en.usa#holiday@group.v.calendar.google.com',
-			color: 'green'
+			color: 'green',
+			eventBorderColor: 'green'
 		},
 		{
-			googleCalendarId: 'santaclara.acm@gmail.com'
+			googleCalendarId: 'santaclara.acm@gmail.com',
+			color: 'blue',
+			eventBorderColor: 'blue'
 		}
 	],
 
@@ -30,6 +34,16 @@ $(document).ready(function() {
 		window.open(event.url, 'gcalevent', 'width=700,height=600');
 		return false;
 	},
+
+	views: {
+		month: {
+
+		},
+		agendaWeek: {
+			titleFormat: 'MMMM YYYY',
+			columnFormat: 'ddd D',
+		}
+	}
 
 	loading: function(bool) {
 		$('#loading').toggle(bool);
