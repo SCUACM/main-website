398a1cda8859bead6bb58e60016e7a1de3a1a8cd
Story DeWeese <sdeweese@scu.edu>
2019-07-14 23:21:38 -0700

    Add space before footer

--- board.php
+++ board.php
@@ -158,6 +158,10 @@
 					</cms:pages>
 				</div>
 				<br />
+				<br />
+				<br />
+				<br />
+				<br />
 				<hr class="title-divider">
 				<div class="row">
 								<div class="connect text-center">
